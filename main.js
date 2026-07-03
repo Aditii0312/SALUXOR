bf5444d13963348ede32bd7b7f4c7db1a3e78fa0
Aditi Kulkarni <aditikulkarniaskk1234@gmail.com>
2026-07-03 09:27:41 +0530

    Improve modal glass effect

--- main.js
+++ main.js
@@ -512,3 +512,26 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 });
+@keyframes popupAppear{
+
+    0 % {
+
+        opacity: 0;
+
+        transform:
+            translateY(18px)
+        scale(.95);
+
+    }
+
+    100 % {
+
+        opacity: 1;
+
+        transform:
+            translateY(0)
+        scale(1);
+
+    }
+
+}
